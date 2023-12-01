89843aff970d68ad7c226f9a77736068ce23bdf6
OBULASETTI VENKANNA BABU <38856533+venkanna831@users.noreply.github.com>
2023-12-01 13:31:43 +0530

    test through code

--- Employee/src/main/java/com/example/demo/controller/EmployeeController.java
+++ Employee/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,44 +1,15 @@
-package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+@Controller
+public class AddController {
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.exception.EmployeeNotFoundException;
-import com.example.demo.model.Employee;
-import com.example.demo.service.EmployeeService;
-
-@RestController
-public class EmployeeController {
-	
-	@Autowired
-	private EmployeeService service;
-	
-	@GetMapping("/employees/{id}")
-	 public Employee getEmployee( @PathVariable int id)
-	 {
-		Employee employee =  service.getEmployeeById(id);
-		if(employee == null)
-			throw new EmployeeNotFoundException("Employee not found");
-		 return employee;
-	 }
-	
-	@GetMapping("/employees")
-	public List<Employee> getAllEmployees()
-	{
-		return service.getAllEmployees();
-	}
-	 
-	@PostMapping("/employees")
-	public int createEmployee(@RequestBody Employee employee)
-	{
-		return service.createEmployee(employee);
-	}
-	 
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ModelAndView add(ModelAndView modelAndView) {
+        modelAndView.addObject("message", "Item added successfully!");
+        modelAndView.setViewName("add");
+        return modelAndView;
+    }
 
 }
