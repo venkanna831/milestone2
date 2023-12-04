4b91af2746a74e5f53a79b65978667f28f1faa2e
OBULASETTI VENKANNA BABU <38856533+venkanna831@users.noreply.github.com>
2023-12-04 16:31:26 +0530

    test through code

---
+++ Employee/src/main/java/com/example/demo/controller/EmployeeController_edit1.java
@@ -0,0 +1,14 @@
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AddController {
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public ModelAndView add(ModelAndView modelAndView) {
+        modelAndView.addObject("message", "Item added successfully!");
+        modelAndView.setViewName("add");
+        return modelAndView;
+    }
+
+}
