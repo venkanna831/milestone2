0467c2a7603cc0014aa59028c68152017c409d46
OBULASETTI VENKANNA BABU <38856533+venkanna831@users.noreply.github.com>
2023-12-05 14:17:56 +0530

    test through code ui

--- Employee/src/main/java/com/example/demo/controller/EmployeeController_edit1.java
+++ Employee/src/main/java/com/example/demo/controller/EmployeeController_edit1.java
@@ -1,14 +1,5 @@
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-@Controller
-public class AddController {
-
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public ModelAndView add(ModelAndView modelAndView) {
-        modelAndView.addObject("message", "Item added successfully!");
-        modelAndView.setViewName("add");
-        return modelAndView;
-    }
-
+java
+@RequestMapping("/register")
+public String loadRegisterPage() {
+return "register";
 }
